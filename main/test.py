334d18cd410ebee02028e68a3631200d69670b09
zhuc <zhuc@esensoft.com>
2018-10-19 10:41:29 +0800

    首次提交

---
+++ main/test.py
@@ -0,0 +1,7 @@
+import sys
+# sys.path.append('./')
+
+# import algorithm.knn as knn
+# print(knn.createDataSet())
+
+print(sys.path)
---
+++ test.py
@@ -0,0 +1,6 @@
+# from algorithm import knn
+import algorithm.knn 
+
+a,b = algorithm.knn.createDataSet()
+
+print(a.shape[0])
